aaf7403066831db4ebc19c1e36880800b05e6d98
Tomasz-Budny <s208246@sggw.edu.pl>
2022-07-13 14:55:35 +0200

    Added method CombinedGradientsArray to GradientTypeManagerCollection

--- Assets/Scripts/GradientTypeManager.cs
+++ Assets/Scripts/GradientTypeManager.cs
@@ -2,9 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GradientSetsCollection
+public class GradientTypeManagerCollection
 {
+    public GradientTypeManager[] gradients;
 
+    public float[,] GetCombinedGradientsArray(int width, int height)
+    {
+        float[,] combinedArray = new float[width, height];
+        float maxHeight = float.MinValue;
+        float minHeight = float.MaxValue;
+
+        foreach(GradientTypeManager gtm in gradients)
+        {
+            Gradient gradient = gtm.GenerateGradientBasedOnGradientType();
+            float[,] gradientMap = gradient.Generate(width, height);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    combinedArray[x, y] += gradientMap[x, y];
+                    float currentHeight = combinedArray[x, y];
+
+                    if (maxHeight < currentHeight)
+                        maxHeight = currentHeight;
+                    if (minHeight > currentHeight)
+                        minHeight = currentHeight;
+                }
+            }
+        }
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                combinedArray[x, y] = Mathf.InverseLerp(minHeight, maxHeight, combinedArray[x, y]);
+            }
+        }
+        return combinedArray;
+    }
 }
 
 
@@ -15,6 +49,7 @@ public class GradientTypeManager
 
     public GradientType gradientType;
     public float figureSize;
+    public Vector2 figurePosition;
 
     public Gradient GenerateGradientBasedOnGradientType()
     {
